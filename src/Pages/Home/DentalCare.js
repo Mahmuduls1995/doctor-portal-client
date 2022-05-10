f594808ff87c9a2500411fc1f292d6e48d4946c7
mahmudul1995 <mahmuduls451@gmail.com>
2022-05-10 23:48:46 +0600

    appointment part complete

--- src/Pages/Home/DentalCare.js
+++ src/Pages/Home/DentalCare.js
@@ -2,12 +2,12 @@ import React from 'react';
 import treatment from '../../assets/images/treatment.png'
 const DentalCare = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row">
-                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <img src={treatment} class="max-w-sm rounded-lg shadow-xl" />
                 <div className="px-4">
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
                 </div>
             </div>
