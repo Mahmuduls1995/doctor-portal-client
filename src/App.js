1942ecccf70f69b115d37565ac6f1fc5f7a448b8
mahmudul1995 <mahmuduls451@gmail.com>
2022-05-10 23:31:36 +0600

    service and banner part complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import './App.css';
 import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
+import Appoinment from "./Pages/Appoinment/Appoinment";
 import Home from "./Pages/Home/Home";
+import Review from "./Pages/Review/Review";
+
 import Navbar from "./Pages/Shared/Navbar";
 function App() {
   return (
@@ -10,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/about" element={<About></About>} />
+        <Route path="/review" element={<Review></Review>} />
+        <Route path="/appoinment" element={<Appoinment></Appoinment>} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Home/DentalCare.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import treatment from '../../assets/images/treatment.png'
+const DentalCare = () => {
+    return (
+        <div class="hero min-h-screen bg-base-200">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <div className="px-4">
+                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DentalCare;
